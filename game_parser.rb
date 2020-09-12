cdaf755e9eee92d71e99476c051378f135fa4c31
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2020-09-12 17:57:14 -0300

    Feature(GameParser): Parse input

---
+++ game_parser.rb
@@ -0,0 +1,24 @@
+require 'stringio'
+
+class GameParser
+  attr_accessor :input, :players_scores
+
+  def initialize
+    @input = $stdin.read
+    @players_scores = {}
+  end
+
+  def parse
+    input.split("\n").each do |turn|
+      player_name, score = turn.split(' ')
+
+      players_scores[player_name] ||= []
+      score = score == 'F' ? score : score.to_i
+      players_scores[player_name] << score
+    end
+  end
+end
+
+# parser = GameParser.new
+
+# p parser.input
---
+++ spec/game_parser_spec.rb
@@ -0,0 +1,62 @@
+require 'stringio'
+require 'byebug'
+require File.expand_path('../game_parser', __dir__)
+
+describe GameParser do
+  before do
+    allow($stdin).to receive(:read).and_return(input_text)
+  end
+
+  subject { described_class.new }
+
+  let(:input_text) do
+    <<~INPUT_TEXT
+    Jeff 10
+    John 3
+    John 7
+    Jeff 7
+    Jeff 3
+    John 6
+    John 3
+    Jeff 9
+    Jeff 0
+    John 10
+    Jeff 10
+    John 8
+    John 1
+    Jeff 0
+    Jeff 8
+    John 10
+    Jeff 8
+    Jeff 2
+    John 10
+    Jeff F
+    Jeff 6
+    John 9
+    John 0
+    Jeff 10
+    John 7
+    John 3
+    Jeff 10
+    John 4
+    John 4
+    Jeff 10
+    Jeff 8
+    Jeff 1
+    John 10
+    John 9
+    John 0
+    INPUT_TEXT
+  end
+
+  describe '#parse' do
+    before { subject.parse }
+
+    it 'should parse the input in games' do
+      expect(subject.players_scores).to eql({
+        'Jeff' => [10, 7, 3, 9, 0, 10, 0, 8, 8, 2, 'F', 6, 10, 10, 10, 8, 1],
+        'John' => [3, 7, 6, 3, 10, 8, 1, 10, 10, 9, 0, 7, 3, 4, 4, 10, 9, 0]
+      })
+    end
+  end
+end
